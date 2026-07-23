28abdfce1189365e0c82e058e7b71d297a870b1c
CandySanjo <CandySanjo@outlook.com>
2026-07-23 15:07:47 +0800

    fix: build script with absolute paths

--- build.sh
+++ build.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 set -e
+DIR="$(cd "$(dirname "$0")" && pwd)"
 echo "1/3 Building frontend..."
-cd front && npm run build && cd ..
+(cd "$DIR/front" && npm run build)
 echo "2/3 Deploying to gateway static..."
-rsync -a --delete front/dist/ gateway/src/main/resources/static/
+rsync -a --delete "$DIR/front/dist/" "$DIR/gateway/src/main/resources/static/"
 echo "3/3 Packaging..."
-./mvnw package -DskipTests
+(cd "$DIR" && ./mvnw package -DskipTests)
 echo "Done. Run: docker compose up --build"
 
